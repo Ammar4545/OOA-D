39189917c1617d320dbe262f8b943a02ac843bc0
Ammar <ammar.m777.2@gmail.com>
2024-10-02 13:24:05 +0300

    add TypeHelper class may be modified

---
+++ CH1.WellDesignedAppsRock/Hepler/TypeHepler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH1.WellDesignedAppsRock.Hepler
+{
+    public class TypeHepler
+    {
+        public static string ToFriendlyString(Type type)
+        {
+            switch (type)
+            {
+                case Type.ACOUSTIC:
+                    return "acoustic";
+                case Type.ELECTRIC:
+                    return "electric";
+                
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(type), type, null);
+            }
+        }
+        
+    }
+    public enum Type
+        {
+           ACOUSTIC,
+           ELECTRIC
+        }
+}
