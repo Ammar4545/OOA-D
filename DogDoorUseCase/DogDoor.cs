d208e94c4a491c2d5d5ba16b8773eba3c381ada7
Ammmar M <ammar.m777@gmail.com>
2024-10-09 13:28:42 +0300

    testing the new version of use case

--- DogDoorUseCase/DogDoor.cs
+++ DogDoorUseCase/DogDoor.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DogDoor
+namespace DogDoorUseCase
 {
     public class DogDoor
     {
--- DogDoorUseCase/Program.cs
+++ DogDoorUseCase/Program.cs
@@ -1,11 +1,23 @@
-﻿
-namespace DogDoorUseCase
+﻿namespace DogDoorUseCase
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DogDoor door = new DogDoor();
+            Remote remote = new Remote(door);
+            Console.WriteLine("Fido barks to go outside...");
+            remote.PressButton();
+            Thread.Sleep(6000);
+
+            Console.WriteLine("Fido has gone outside...");
+          
+            Console.WriteLine("Fido’s all done...");
+         
+            Console.WriteLine("Fido’s back inside...");
+
+            var opened = door.IsOpen();
+            Console.WriteLine(opened);
         }
     }
 }
