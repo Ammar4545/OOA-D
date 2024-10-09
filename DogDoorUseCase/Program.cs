14ca27a4c6bb626d57ae9a710a8377b267601c86
Ammmar M <ammar.m777@gmail.com>
2024-10-09 11:50:36 +0300

    add new project following use case after gathering requirements

---
+++ DogDoorUseCase/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace DogDoorUseCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
