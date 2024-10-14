292063c88db1101229509de1ef25af5e9940fed1
Ammar4545 <ammar.m777.2@gmail.com>
2024-10-14 14:32:57 +0300

    make class for test wit solving last issue related to door status

--- CH3-Requirements Change/DogDoorSimulator.cs
+++ CH3-Requirements Change/DogDoorSimulator.cs
@@ -8,5 +8,32 @@ namespace CH3_Requirements_Change
 {
     public class DogDoorSimulator
     {
-    }
+        public static void DogDoorTestPath()
+        {
+            DogDoor door = new DogDoor();
+            Remote remote = new Remote(door);
+            //var opened=false;
+            Console.WriteLine("Fido barks to go outside for 6s");
+            remote.PressButton();
+           
+            Console.WriteLine($"is Door opend {door.IsOpen()}");
+
+            Console.WriteLine("Fido has gone outside...");
+
+            Console.WriteLine("Fido’s all done...");
+
+            //you see that door still open for 5s but fido take 6s now door is closed
+            Thread.Sleep(6000);
+
+            Console.WriteLine("Fido stuck ouside");
+            Console.WriteLine("Fido barking ...");
+            door.IsOpen(); //false
+            Console.WriteLine($"is Door opend {door.IsOpen()}");
+            Console.WriteLine("Gina use the remote contoll to open the door.");
+
+            remote.PressButton();
+            Console.WriteLine($"is Door opend {door.IsOpen()}");
+            Console.WriteLine("Fido has back insid...");
+        }
+     }
 }
