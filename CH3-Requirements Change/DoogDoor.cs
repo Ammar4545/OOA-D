edc0046f35978eaec414b452af7fe2a6fe4c46ae
Ammar4545 <ammar.m777.2@gmail.com>
2024-10-14 14:34:07 +0300

    solve issue with DogDoor class update door status for every call

--- CH3-Requirements Change/DoogDoor.cs
+++ CH3-Requirements Change/DoogDoor.cs
@@ -15,10 +15,12 @@ namespace CH3_Requirements_Change
         }
         public void Open()
         {
+            open = true;
             Console.WriteLine("The dog door opens");
         }
         public void Close()
         {
+            open = false;
             Console.WriteLine("The dog door closes");
         }
         public bool IsOpen() => this.open;
