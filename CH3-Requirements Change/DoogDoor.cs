77df88fc0d96ab7bc78f323a1bca439df7c86fb7
Ammar4545 <ammar.m777.2@gmail.com>
2024-10-14 12:26:47 +0300

    add DoogDoor class

---
+++ CH3-Requirements Change/DoogDoor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH3_Requirements_Change
+{
+    public class DogDoor
+    {
+        private bool open;
+        public DogDoor()
+        {
+            this.open = false;
+        }
+        public void Open()
+        {
+            Console.WriteLine("The dog door opens");
+        }
+        public void Close()
+        {
+            Console.WriteLine("The dog door closes");
+        }
+        public bool IsOpen() => this.open;
+    }
+}
