96d148e5cac222ed7fb13c8bb3ed13fd76b7d099
Ammar4545 <ammar.m777.2@gmail.com>
2024-10-16 10:03:55 +0300

    add DogDoor class for new ch 4

---
+++ CH4-Analysis/DogDoor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH4_Analysis
+{
+    public class DogDoor
+    {
+        private bool open;
+        public DogDoor()
+        {
+            this.open = false;
+        }
+        public void Open()
+        {
+            open = true;
+            Console.WriteLine("The dog door opens");
+            Timer timer = new Timer(CloseDoor, null, 5000, Timeout.Infinite);
+        }
+        public void Close()
+        {
+            open = false;
+            Console.WriteLine("The dog door closes");
+        }
+        public bool IsOpen() => this.open;
+        private void CloseDoor(object state)
+        {
+            open = false;
+        }
+    }
+}
