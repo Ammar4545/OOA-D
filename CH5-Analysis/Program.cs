79130b56968058510a20dba90bd172afc5394747
Ammar4545 <ammar.m777.2@gmail.com>
2024-10-21 09:00:09 +0300

    add new project for chapter 5

---
+++ CH5-Analysis/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CH5_Analysis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
