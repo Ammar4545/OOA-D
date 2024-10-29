8964d3c6f799070a1ff55c05d8ac17877640988f
Ammar4545 <ammar.m777.2@gmail.com>
2024-10-29 10:46:17 +0300

    add instrument class

---
+++ CH5-Analysis/Instrument.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH5_Analysis
+{
+    public abstract class Instrument
+    {
+        internal string serialNumber { get; }
+        internal double price { get; set; }
+        public InstrumentSpec spec{get; set;}
+
+        public Instrument(string serialNumber, double price, InstrumentSpec spec)
+        {
+            this.serialNumber = serialNumber;
+            this.price = price;
+            this.spec = spec;
+        }
+
+    }
+}
